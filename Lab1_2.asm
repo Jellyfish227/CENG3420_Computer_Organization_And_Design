9e9171f6647acfa596f5ca5183fe2a4cfd6a46bb
Jellyfish_Yu <mrbrain699@gmail.com>
2024-01-18 14:33:18 +0800

    Finished both question of Lab1

--- Lab1_2.asm
+++ Lab1_2.asm
@@ -13,6 +13,13 @@ _start:
     mv t1, a0
     ecall
     mv t2, a0
+
+    #modify integer
+    addi t0, t0, 3
+    li t3, 2
+    mul t1, t1, t3
+    add t2, t2, t0
+    add t2, t2, t1
     
     #out int
     li a7, 1
@@ -26,8 +33,18 @@ _start:
     li a7, 64
     ecall
     
+    li a7, 1 # mode prtInt
     mv a0, t1
     ecall
+
+    #newline
+    li a0, 1 # 1 = STDOUT
+    la a1, endl
+    li a2, 1
+    li a7, 64 #mode write
+    ecall
+
+    li a7, 1 #mode prtInt
     mv a0, t2
     ecall
     
